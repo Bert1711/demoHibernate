00504ead9c8f111f6f1444077bd921a466484e9f
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-06-29 22:20:08 +0300

    "add Person class"

---
+++ src/main/java/ru/zaroyan/demohibernate/models/Person.java
@@ -0,0 +1,35 @@
+package ru.zaroyan.demohibernate.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "city_of_living")
+    private String cityOfLiving;
+
+
+}
