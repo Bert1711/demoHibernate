353a786f405350e6bcffe25c220c0012462c2f68
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-07-02 21:27:01 +0300

    "change Person class"

--- src/main/java/ru/zaroyan/demohibernate/models/Person.java
+++ src/main/java/ru/zaroyan/demohibernate/models/Person.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Check;
 
 @Data
 @Builder
@@ -16,18 +17,21 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", length = 150)
     private String name;
 
-    @Column(name = "surname")
+    @Column(name = "surname", length = 150)
     private String surname;
 
     @Column(name = "age")
+    @Check(constraints = "age >= 0 AND age < 150")
     private int age;
 
-    @Column(name = "phone_number")
+    @Column(name = "phone_number", length = 20)
+    @Check(constraints = "phone_number ~ '^[0-9+\\- ()]+$' AND CHAR_LENGTH(phone_number) >= 7")
     private String phoneNumber;
 
     @ManyToOne(optional = false)
     private City cityOfLiving;
 }
+
