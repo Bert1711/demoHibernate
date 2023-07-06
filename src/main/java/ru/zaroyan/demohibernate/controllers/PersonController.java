496ce8137d4cbaf0321d391c4da13a0ab2e147b3
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-07-06 18:21:00 +0300

    "change PersonController"

--- src/main/java/ru/zaroyan/demohibernate/controllers/PersonController.java
+++ src/main/java/ru/zaroyan/demohibernate/controllers/PersonController.java
@@ -1,15 +1,15 @@
 package ru.zaroyan.demohibernate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.zaroyan.demohibernate.models.Person;
 import ru.zaroyan.demohibernate.repository.PersonRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/persons")
 public class PersonController {
     private final PersonRepository personRepository;
 
@@ -18,8 +18,26 @@ public class PersonController {
         this.personRepository = personRepository;
     }
 
-    @GetMapping("/persons/by-city")
-    public List<Person> getPersonsByCity(@RequestParam("city") String city) {
-        return personRepository.findByCityOfLiving(city);
+    @GetMapping("")
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+
+    @GetMapping("/by-city")
+    public List<Person> getPersonsByCity(@RequestParam String city) {
+        return personRepository.findByCityName(city);
+    }
+
+
+    @GetMapping("/age")
+    public List<Person> getPersonsByAgeLessThan(@RequestParam int age) {
+        return personRepository.findByAgeLessThanOrderByAgeAsc(age);
+    }
+
+    @GetMapping("/name")
+    public Optional<Person> getPersonByNameAndSurname(
+            @RequestParam String name, @RequestParam String surname) {
+        return personRepository.findByNameAndSurname(name, surname);
     }
 }
+
