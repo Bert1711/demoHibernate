3bad373c01aad62d0a2fcca5694b021f09133dea
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-07-06 17:49:35 +0300

    "change PersonRepository"

--- src/main/java/ru/zaroyan/demohibernate/repository/PersonRepository.java
+++ src/main/java/ru/zaroyan/demohibernate/repository/PersonRepository.java
@@ -1,34 +1,18 @@
 package ru.zaroyan.demohibernate.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.zaroyan.demohibernate.models.Person;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public class PersonRepository {
-    @PersistenceContext
-    private EntityManager entityManager;
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    List<Person> findByCityName(String cityName);
 
-    public List<Person> findByCityOfLiving(String city) {
-        return entityManager.createQuery(
-                "SELECT p FROM Person p WHERE p.city.name = :city", Person.class)
-                .setParameter("city", city)
-                .getResultList();
-    }
+    List<Person> findByAgeLessThanOrderByAgeAsc(int age);
+
+    Optional<Person> findByNameAndSurname(String name, String surname);
 }
 
-
-/**
- * Аннотация @PersistenceContext в Spring и JPA используется для инъекции (внедрения) EntityManager в класс или бин.
- * EntityManager представляет собой основной интерфейс для работы с объектами в контексте хранения JPA
- * и управления персистентностью данных.
- *
- * Когда вы помечаете поле или метод с аннотацией @PersistenceContext,
- * Spring автоматически внедряет EntityManager в это поле или вызывает метод
- * с аргументом EntityManager при создании экземпляра класса или бина.
- * Это позволяет вам использовать EntityManager для выполнения операций базы данных,
- * таких как сохранение, загрузка, обновление и удаление сущностей.
- */
